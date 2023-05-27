35201788c1711905201fca8f6efc4d9a9ba7cd4e
Daniel-J101 <98334700+Daniel-J101@users.noreply.github.com>
2023-05-27 13:57:23 -0500

    add dependencies

--- index.js
+++ index.js
@@ -7,8 +7,19 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const {onRequest} = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
+const functions = require("firebase-functions");
+const { PKPass } = require('passkit-generator');
+var fs = require('file-system');
+var path = require('path');
+var axios = require('axios');
+
+exports.pass = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.send("Hello from Firebase");
+})
+
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
